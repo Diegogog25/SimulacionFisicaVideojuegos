3d45a178e489ba5fc6b1c50b0ba21a080cdb590a
Diegogog25 <diegog25@ucm.es>
2026-09-18 11:27:13 +0200

    P0 hasta actividad 3

--- skeleton/Vector3D.cpp
+++ skeleton/Vector3D.cpp
@@ -31,3 +31,36 @@ Vector3D
 Vector3D::cross(const Vector3D& v) const{
 	return Vector3D(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
 }
+
+Vector3D
+Vector3D::operator+(const Vector3D& v) const
+{
+	return Vector3D(x + v.x, y + v.y, z + v.z);
+}
+
+Vector3D
+Vector3D::operator-(const Vector3D& v) const 
+{
+	return Vector3D(x - v.x, y - v.y, z - v.z);
+}
+
+Vector3D
+Vector3D::operator*(float f) const {
+	return Vector3D(x * f, y * f, z * f);
+}
+
+Vector3D
+Vector3D::operator=(const Vector3D& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+	return *this;
+}
+
+Vector3D
+Vector3D::operator+=(const Vector3D& v) {
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	return *this;
+}
--- skeleton/Vector3D.h
+++ skeleton/Vector3D.h
@@ -16,8 +16,19 @@ public:
 	Vector3D normalize() const;
 	float dot(const Vector3D& v) const;
 	Vector3D cross(const Vector3D& v) const;
+	
+	//Operator
+	Vector3D operator+(const Vector3D& v) const;
+	Vector3D operator-(const Vector3D& v) const;
+	Vector3D operator*(const float f) const;
+	Vector3D operator=(const Vector3D& v);
+	Vector3D operator+=(const Vector3D& v);
 
+	operator physx::PxVec3() const {
+		return physx::PxVec3(x, y, z);
+	}
 
+	//Variables
 	float x, y, z;
 };
 
